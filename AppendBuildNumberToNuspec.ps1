fd8c3d5e350ad9bb825c985c52168f141bfaae48
Claudio Eduardo Steuernagel <claudio.steuernagel@becomex.com.br>
2018-06-19 11:33:05 -0300

    Ajustes scripts

--- AppendBuildNumberToNuspec.ps1
+++ AppendBuildNumberToNuspec.ps1
@@ -4,9 +4,8 @@ Param (
     [Parameter(Mandatory=$true)]
     [string] $Build
 )
-#$NuspecPath = 'C:\Users\claudio\source\repos\ClassLibrary\ClassLibrary\ClassLibrary.nuspec'
-$ErrorActionPreference = 'Stop'  # Stops executing on error instead of silent continue.
-Set-StrictMode -Version Latest   # Enforces coding rules in expressions, scripts, and script blocks. Uninitialized variables are not permitted.
+
+#$ErrorActionPreference = 'Stop'  # Stops executing on error instead of silent continue.
 
 $fileContent = Get-Content $NuspecPath | Out-String
 
@@ -14,10 +13,21 @@ $nuspecVersionPattern = '(\<version\>)(\d+\.\d+\.\d+(\.\d)?)(\<\/version\>)'
 
 $buildVersionPattern = '([^\s]+)_(\d+)'
 $currentBuildStr = [RegEx]::Match($Build, $buildVersionPattern).Groups[2].Value
+if(!$currentBuildStr)
+{
+    $currentBuildStr = $Build
+}
 
 $currentVersionStr = [RegEx]::Match($fileContent, $nuspecVersionPattern).Groups[2].Value
 $newVersionStr = $currentVersionStr + "." + $currentBuildStr
-
 $fileContent = [RegEx]::Replace($fileContent, $nuspecVersionPattern, '${1}' + $newVersionStr + '${4}' )
 
-$fileContent | Out-File $AssemblyInfoPath
+$dependencyVersionPattern = '(\<dependency id="[^\s]+" version=")('+$currentVersionStr+')("\s?\/>)'
+$dependencyVersionStr = [RegEx]::Match($fileContent, $dependencyVersionPattern).Groups[2].Value
+if($dependencyVersionStr)
+{
+    $newdependencyVersionStr = $dependencyVersionStr + "." + $currentBuildStr
+    $fileContent = [RegEx]::Replace($fileContent, $dependencyVersionPattern, '${1}' + $newdependencyVersionStr + '${3}' )
+}
+
+$fileContent | Out-File $NuspecPath
